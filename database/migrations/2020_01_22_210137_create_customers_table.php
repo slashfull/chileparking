682e988f09823296007998240480c6af63a032f9
Diego Pacaje <mc.diegox.95@gmail.com>
2020-01-25 22:48:39 -0300

    modificacion de tabla users y renombre de tabla persons a customers

---
+++ database/migrations/2020_01_22_210137_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->engine = 'innoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+            $table->bigIncrements('id');
+            $table->string('rut', 9)->unique();
+            $table->string('name', 30);
+            $table->string('lastname', 30);
+            $table->integer('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
